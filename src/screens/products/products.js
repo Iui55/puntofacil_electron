938a9f108cb2b1ac4ef820a3eb2bf692b9be6fe8
Gio Arellano <cuzztomgdev@gmail.com>
2025-09-26 15:06:14 -0600

    feature: use searchbar component

--- src/screens/products/products.js
+++ src/screens/products/products.js
@@ -1,24 +1,31 @@
-const { BrowserWindow } = require("electron").remote;
-const path = require("path");
+(() => {
+  const path = require("path");
+  // console.log("Products screen loaded");
+  // const { BrowserWindow } = require("electron").remote;
+  const SearchBar = require("../components/searchBar/searchBar.js");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const btnRegister = document.getElementById("registerProductBtn");
-
-  btnRegister.addEventListener("click", () => {
-    const modal = new BrowserWindow({
-      width: 650,
-      height: 550,
-      parent: require("electron").remote.getCurrentWindow(),
-      modal: true,
-      frame: false,
-      resizable: false,
-      backgroundColor: "#00000000", // transparente
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-      },
-    });
-
-    modal.loadFile(path.join(__dirname, "product-form/product-form.html"));
+  const sbProducts = new SearchBar({
+    container: document.getElementById("searchBarProducts"),
+    placeholder: "Buscar productos",
   });
-});
+  // const btnRegister = document.getElementById("registerProductBtn");
+
+  // btnRegister.addEventListener("click", () => {
+  //   const modal = new BrowserWindow({
+  //     width: 650,
+  //     height: 550,
+  //     parent: require("electron").remote.getCurrentWindow(),
+  //     modal: true,
+  //     frame: false,
+  //     resizable: false,
+  //     backgroundColor: "#00000000", // transparente
+  //     webPreferences: {
+  //       nodeIntegration: true,
+  //       contextIsolation: false,
+  //     },
+  //   });
+
+  //   modal.loadFile(path.join(__dirname, "product-form/product-form.html"));
+  // });
+
+})();
