4b4697ab78fbf572d6a47627638f1e1ddbef9851
Gio Arellano <cuzztomgdev@gmail.com>
2025-09-26 18:10:59 -0600

    feature: dynamic table base

---
+++ src/screens/components/tableData/tableData.js
@@ -0,0 +1,107 @@
+class TableData {
+  constructor(options) {
+    this.container = options.container;
+    
+    this.headers = options.headers || []; // Array of { label: "Header", key: "dataKey" }
+    this.actions = options.actions || []; // Array of { label: "Action", class: "CSS class", onClick: (row) => {} }
+    
+    if (this.actions.length > 0) 
+      this.headers.push({ label: "Acciones", key: "__actions" });
+
+    //
+    this.onSelect = options.onSelect || ((item) => {});
+    this.data = options.data || []; // Array of data objects
+
+    this._build();
+  }
+
+  
+  async _build() {
+    // Load Searchbar HTML
+    const res = await fetch("../components/tableData/tableData.html");
+    const html = await res.text();
+    this.container.innerHTML = html.trim();
+
+    // Setup elements
+    const headerEl = this.container.querySelector(".table-header");
+
+    // Render headers
+    headerEl.innerHTML = "";
+    this.headers.forEach((h) => {
+      const col = document.createElement("div");
+      col.className = "col";
+      col.textContent = h.label;
+      headerEl.appendChild(col);
+    });
+    this.setData(this.data || []);
+
+    // searchInput.placeholder = this.placeholder;
+
+    // Setup events
+    // searchInput.addEventListener("input", (e) => {
+    //   const value = (e.target.value || "").trim().toLowerCase();
+    //   this.onSearch(value);
+    // });
+
+    // this.container.querySelector(".clear-btn").addEventListener("click", () => {
+    //   searchInput.value = "";
+    //   this.onClear();
+    // });
+  }
+
+  // renderHeaders(headers) {
+  //   this.headers = headers || [];
+
+  //   return headers.map((header) => {
+  //     const col = document.createElement("div");
+  //     col.className = "col";
+  //     col.textContent = h.label;
+  //     return col;
+  //   });
+  // }
+  
+  setData(data) {
+    const bodyEl = this.container.querySelector(".table-body");
+    this.data = data || [];
+    
+    bodyEl.innerHTML = "";
+    if (this.data.length === 0) {
+      bodyEl.innerHTML = `<div style="color:#6a7a9a">No hay datos para mostrar.</div>`;
+      return;
+    }
+
+    this.data.forEach((rowData, rowIndex) => {
+      const row = document.createElement("div");
+      row.className = "table-row";
+      row.addEventListener("click", () => {
+        this.onSelect(rowData);
+      });
+
+      this.headers.forEach((h) => {
+        const col = document.createElement("div");
+        col.className = "col";
+        if (h.key === "__actions") {
+          // Render action buttons
+          this.actions.forEach((action) => {
+            const btn = document.createElement("button");
+            btn.className =  `action-btn ${action.class || ""}`;
+            btn.textContent = action.label || "Action";
+            btn.addEventListener("click", (e) => {
+              e.stopPropagation(); // to avoid triggering row click
+              action.onClick(rowData, rowIndex);
+            });
+            col.appendChild(btn);
+          });
+        } else {
+          col.textContent = rowData[h.key] || "";
+        }
+        row.appendChild(col);
+      });
+
+      bodyEl.appendChild(row);
+    });
+  }
+
+}
+
+module.exports = TableData;
--- src/screens/products/products.js
+++ src/screens/products/products.js
@@ -3,11 +3,40 @@
   // console.log("Products screen loaded");
   // const { BrowserWindow } = require("electron").remote;
   const SearchBar = require("../components/searchBar/searchBar.js");
+  const TableData = require("../components/tableData/tableData.js");
 
   const sbProducts = new SearchBar({
     container: document.getElementById("searchBarProducts"),
     placeholder: "Buscar productos",
   });
+
+
+  // ----- Estado -----
+  const SAMPLE_PRODUCTS = [];
+  // Generar 60 productos de prueba
+  for (let i = 1; i <= 60; i++) {
+    SAMPLE_PRODUCTS.push({
+      name: i % 4 === 0 ? `Refresco 600 ml. ${i}` : `Producto ${i}`,
+      id: i,
+      stock: Math.floor(5 + (i % 10) * 3),
+      price: i % 5 === 0 ? 12.5 + (i % 3) * 5 : 25.0,
+      cost: i % 3 === 0 ? 10.0 + (i % 4) * 4 : 15.0,
+    });
+  }
+
+  const productsTable = new TableData({
+    container: document.getElementById("productsTable"),
+    headers: [
+      { label: "Nombre producto", key: "name" },
+      { label: "Código de barras", key: "id" },
+      { label: "Stock", key: "stock" },
+      { label: "Precio compra", key: "cost" },
+      { label: "Precio venta", key: "price" },
+    ],
+    data: SAMPLE_PRODUCTS,
+  });
+
+
   // const btnRegister = document.getElementById("registerProductBtn");
 
   // btnRegister.addEventListener("click", () => {
