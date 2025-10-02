d76dc58989a619b118330c3143c32128338b7738
Gio Arellano <cuzztomgdev@gmail.com>
2025-10-02 16:02:02 -0600

    feature: home listening notifications

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -5,28 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("overlay").style.display = "none";
   });
 
+  // ---- UI Components ----
   const content = document.getElementById("content");
-
-  const loadView = async (view) => {
-    mainPath = `../${view}/`;
-    const res = await fetch(`${mainPath}${view}.html`);
-    const html = await res.text();
-    content.innerHTML = html;
-
-    // Dynamic scripts
-    const scripts = content.querySelectorAll("script");
-    scripts.forEach((oldScript) => {
-      const newScript = document.createElement("script");
-      newScript.src = `${mainPath}${view}.js`;
-      document.body.appendChild(newScript);
-      oldScript.remove();
-    });
-  };
+  const notificationsContainer = document.getElementById("push-notifications");
 
   // Navigation buttons
   const salesBtn = document.getElementById("salesBtn");
   const productsBtn = document.getElementById("productsBtn");
   const newSaleBtn = document.getElementById("newSaleBtn");
+
+
+  // ---- Renderers listeners ----
+  ipcRenderer.on("app:notification", (event, message) => {
+    const toast = document.createElement("div");
+    toast.classList.add("notification");
+    toast.textContent = message;
+
+    notificationsContainer.appendChild(toast);
+    
+    // hide after
+    setTimeout(() => {
+      toast.style.animation = "slideOut 0.3s forwards";
+      toast.addEventListener("animationend", () => toast.remove());
+    }, 3000);
+  });
+
+  // ---- Listeners Events ----
   salesBtn.addEventListener("click", () => {
     loadView("sales");
     productsBtn.classList.remove("active");
@@ -48,6 +52,23 @@ document.addEventListener("DOMContentLoaded", () => {
     newSaleBtn.classList.add("active");
   });
 
-  // Init view
+  // ---- Logic ----
+  const loadView = async (view) => {
+    mainPath = `../${view}/`;
+    const res = await fetch(`${mainPath}${view}.html`);
+    const html = await res.text();
+    content.innerHTML = html;
+
+    // Dynamic scripts
+    const scripts = content.querySelectorAll("script");
+    scripts.forEach((oldScript) => {
+      const newScript = document.createElement("script");
+      newScript.src = `${mainPath}${view}.js`;
+      document.body.appendChild(newScript);
+      oldScript.remove();
+    });
+  };
+
+  // Initial view
   loadView("products");
 });
